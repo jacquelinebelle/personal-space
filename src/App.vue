b1302b666f71ae05a707326180904f1a7a589d43
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-09-16 09:01:05 -0600

    Import Rhino Sans font for h1 in header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Header />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header">
+    <h1>Personal Space</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+@font-face {
+  font-family: "Rhino Sans";
+  src: url("../assets/rhinosans.ttf") format("truetype");
+} 
+
+
+.select-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h1 {
+  font-size: 3rem;
+  font-family: "Rhino Sans";
+
+}
+
+</style>
