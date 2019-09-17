3907508d389391272abdbeddce49bfa80f3c470a
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-09-17 12:52:53 -0600

    Route links to day and month views

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DayView from '@/components/DayView'
+import MonthView from '@/components/MonthView'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'DayView',
+      component: DayView
+    },
+    {
+      path: '/month',
+      name: 'MonthView',
+      component: MonthView
+    }
+  ]
+})
